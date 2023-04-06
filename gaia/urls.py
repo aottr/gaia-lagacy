4663266e7bce6368396d4fab146796cd87066b9e
A.Ottr <alex@tailbyte.org>
2023-04-06 19:46:31 +0200

    serve media files in debug

--- gaia/urls.py
+++ gaia/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('ark/', include('ark.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
