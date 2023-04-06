ee9196e517abcce0c4b3f30872f3e9f4b41354a0
A.Ottr <alex@tailbyte.org>
2023-04-06 22:36:50 +0200

    add File admin

--- ark/admin.py
+++ ark/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Species, Animal
+from .models import Species, Animal, File
 
 admin.site.register(Species)
 admin.site.register(Animal)
+admin.site.register(File)
