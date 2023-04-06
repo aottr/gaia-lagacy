08cae084d7de4d0c3049c0a6643839b0cbbf5791
A.Ottr <alex@tailbyte.org>
2023-04-06 19:46:12 +0200

    add detail view and enhance list

--- ark/urls.py
+++ ark/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import StartView
+from .views import StartView, AnimalListView, AnimalDetailView
 
 urlpatterns = [
     path('', StartView.as_view(), name='start'),
+    path('animals', AnimalListView.as_view(), name='animal-list'),
+    path('animals/<pk>', AnimalDetailView.as_view(), name='animal-detail'),
 ]
--- ark/views.py
+++ ark/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from ark.models import Animal
 
 
 class StartView(TemplateView):
     template_name = 'ark/start.html'
+
+
+class AnimalListView(ListView):
+    model = Animal
+    paginate_by = 50
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Gender'] = Animal.Gender
+        context['Gender'].do_not_call_in_templates = True
+        return context
+
+
+class AnimalDetailView(DetailView):
+    # specify the model to use
+    model = Animal
