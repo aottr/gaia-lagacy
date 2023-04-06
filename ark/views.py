26b81a0acb146a35bc7b558ca2caad83f6e9cb83
A.Ottr <alex@tailbyte.org>
2023-04-06 22:37:21 +0200

    add detail view and filters

--- ark/views.py
+++ ark/views.py
@@ -10,7 +10,20 @@ class StartView(TemplateView):
 
 class AnimalListView(ListView):
     model = Animal
-    paginate_by = 50
+
+    def get_queryset(self):
+        context = Animal.objects.all()
+
+        if self.request.GET.get('gender'):
+            gender = self.request.GET.get('gender')
+            context = context.filter(gender=gender)
+
+        if self.request.GET.get('species'):
+            species = self.request.GET.get('species')
+            context = context.filter(species=species)
+
+        order = self.request.GET.get('order', '-pk')
+        return context.order_by(order)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,3 +35,9 @@ class AnimalListView(ListView):
 class AnimalDetailView(DetailView):
     # specify the model to use
     model = Animal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Gender'] = Animal.Gender
+        context['Gender'].do_not_call_in_templates = True
+        return context
